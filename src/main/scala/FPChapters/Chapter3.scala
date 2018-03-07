6507106fe9a5101e182efc6cf8e00abc27eafec9
Arnas Janulionis <arnas.janulionis@gmail.com>
2018-03-07 10:17:15 +0200

    custom list additional methods

--- src/main/scala/FPChapters/Chapter3.scala
+++ src/main/scala/FPChapters/Chapter3.scala
@@ -14,7 +14,6 @@ object Chapter3 {
 
     def product(ds: List[Double]): Double = ds match {
       case Nil => 1
-      case Cons(0.0, _) => 0.0
       case Cons(x, xs) => x * product(xs)
     }
 
@@ -45,9 +44,26 @@ object Chapter3 {
     case _ => l
   }
 
+  def dropWhile2[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile2(t)(f)
+    case _ => l
+  }
+
   def init[A](l: List[A]): List[A] = l match {
     case Nil => sys.error("init on empty list")
     case Cons(_, Nil) => Nil
     case Cons(h, t) => Cons(h, init(t))
   }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]): Int =
+    foldRight(ns, 0)(_ + _)
+
+  def product2(ns: List[Double]): Double =
+    foldRight(ns, 1.0)(_ * _)
 }
